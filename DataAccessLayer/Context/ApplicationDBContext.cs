0c167a3072194d4f7f08dfd4950b3776b965dce0
Mahmoud Mohamed <mahmoud.behidy@gmail.com>
2024-06-03 06:39:19 +0300

    add product and category entities

---
+++ DataAccessLayer/Context/ApplicationDBContext.cs
@@ -0,0 +1,20 @@
+﻿using DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Context
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ DataAccessLayer/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Required]
+        [Range(0, double.MaxValue)] // Ensures non-negative price
+        public decimal Price { get; set; }
+        [ForeignKey("Category")]
+        public int? CategoryId { get; set; } // Foreign key for Category relationship
+        public virtual Category Category { get; set; } // Navigation property for Category
+    }
+}
