1e9a462311444a68e40f1b0f6aae88a72899f0ce
Mahmoud Mohamed <mahmoud.behidy@gmail.com>
2024-06-03 07:23:26 +0300

    add IGenericRepository and GenericRepository

---
+++ BusinessLogicLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLogicLayer.Interfaces;
+using DataAccessLayer.Context;
+using DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        public ApplicationDBContext _context { get; }
+
+        public GenericRepository(ApplicationDBContext applicationDBContext)
+        {
+            _context = applicationDBContext;
+        }
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+        public async Task<int> InsertAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            return await _context.SaveChangesAsync();
+        }
+        public async Task<int> UpdateAsync(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            return await _context.SaveChangesAsync();
+        }
+        public async Task<int> DeleteAsync(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
